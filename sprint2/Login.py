2a34fff7a0707ea23d742cf37efc44ec14c80a3e
getlanced <henrymc381@gmail.com>
2019-07-17 01:35:56 +0800

    Update Login.py
    -Minor change

--- sprint2/Login.py
+++ sprint2/Login.py
@@ -152,7 +152,8 @@ class Log_In(object):
     def PassCheck(self):
         pw = self.LineEdit.text()
         mess = dl.isMatch(pw)
-        if (pw == "Incorrect Password"):
+        #if (pw == "Incorrect Password"):
+        if (mess == ""):
             self.label_3.setText("Incorrect Password")      
         else:
             self.label_3.setText("Welcome "+ mess)
