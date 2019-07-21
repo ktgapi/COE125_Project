ab58727c882ec90c4a18173e795a5b2efe7ee028
ktgapi <kevin.gapi@gmail.com>
2019-07-22 06:41:21 +0800

    add DAL unit test and revised businesslogic

--- sprint3/businesslogic.py
+++ sprint3/businesslogic.py
@@ -1,94 +1,160 @@
-import numpy as np
 import torch
 import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 import matplotlib.pyplot as plt
-from time import time
-from torchvision import datasets, transforms
-from torch import nn, optim
 
-transform = transforms.Compose([transforms.ToTensor(),
-                              transforms.Normalize((0.5,), (0.5,)),
-                              ])
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
 
-trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
-valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
 
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
 
-input_size = 784
-hidden_sizes = [128, 64]
-output_size = 10
+n_epochs = 3
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
 
-model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-                      nn.ReLU(),
-                      nn.Linear(hidden_sizes[1], output_size),
-                      nn.LogSoftmax(dim=1))
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
 
-criterion = nn.NLLLoss()
-images, labels = next(iter(trainloader))
-images = images.view(images.shape[0], -1)
+network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                                momentum=momentum)
 
-logps = model(images) #log probabilities
-loss = criterion(logps, labels) #calculate the NLL loss
-"""
-optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
-time0 = time()
-epochs = 15
-for e in range(epochs):
-    running_loss = 0
-    for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
-        images = images.view(images.shape[0], -1)
-    
-        # Training pass
-        optimizer.zero_grad()
-        
-        output = model(images)
-        loss = criterion(output, labels)
-        
-        #This is where the model learns by backpropagating
-        loss.backward()
-        
-        #And optimizes its weights here
-        optimizer.step()
-        
-        running_loss += loss.item()
-    else:
-        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
-	
-print("\nTraining Time (in minutes) =",(time()-time0)/60)"""
+network_state_dict = torch.load('./results/model.pth')
+network.load_state_dict(network_state_dict)
 
-def view_classify(img, ps):
-    ''' Function for viewing an image and it's predicted classes.
-    '''
-    ps = ps.data.numpy().squeeze()
+optimizer_state_dict = torch.load('./results/optimizer.pth')
+optimizer.load_state_dict(optimizer_state_dict)
 
-    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
-    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
-    ax1.axis('off')
-    ax2.barh(np.arange(10), ps)
-    ax2.set_aspect(0.1)
-    ax2.set_yticks(np.arange(10))
-    ax2.set_yticklabels(np.arange(10))
-    ax2.set_title('Class Probability')
-    ax2.set_xlim(0, 1.1)
-    plt.tight_layout()
-    plt.show()
+train_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('files', train=True, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_train, shuffle=True)
 
-images, labels = next(iter(valloader))
+test_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.MNIST('files', train=False, download=True,
+                             transform=torchvision.transforms.Compose([
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
 
-img = images[0].view(1, 784)
-# Turn off gradients to speed up this part
+input_loader = torch.utils.data.DataLoader(
+  torchvision.datasets.ImageFolder('images', 
+                             transform=torchvision.transforms.Compose([
+			       torchvision.transforms.Resize((28,28)),
+			       torchvision.transforms.Grayscale(),
+                               torchvision.transforms.ToTensor(),
+                               torchvision.transforms.Normalize(
+                                 (0.1307,), (0.3081,))
+                             ])),
+  batch_size=batch_size_test, shuffle=True)
+
+examples = enumerate(input_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+'''
+network = Net()
+optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+                      momentum=momentum)
+'''
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+def train(epoch):
+  network.train()
+  for batch_idx, (data, target) in enumerate(train_loader):
+    optimizer.zero_grad()
+    output = network(data)
+    loss = F.nll_loss(output, target)
+    loss.backward()
+    optimizer.step()
+    if batch_idx % log_interval == 0:
+      print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+        epoch, batch_idx * len(data), len(train_loader.dataset),
+        100. * batch_idx / len(train_loader), loss.item()))
+      train_losses.append(loss.item())
+      train_counter.append(
+        (batch_idx*64) + ((epoch-1)*len(train_loader.dataset)))
+      torch.save(network.state_dict(), './results/model.pth')
+      torch.save(optimizer.state_dict(), './results/optimizer.pth')
+
+def test():
+  network.eval()
+  test_loss = 0
+  correct = 0
+  with torch.no_grad():
+    for data, target in test_loader:
+      output = network(data)
+      test_loss += F.nll_loss(output, target, size_average=False).item()
+      pred = output.data.max(1, keepdim=True)[1]
+      correct += pred.eq(target.data.view_as(pred)).sum()
+  test_loss /= len(test_loader.dataset)
+  test_losses.append(test_loss)
+  print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+    test_loss, correct, len(test_loader.dataset),
+    100. * correct / len(test_loader.dataset)))
+'''
+for i in range(51,70):
+  test_counter.append(i*len(train_loader.dataset))
+  train(i)
+  test()
+'''
+'''
+test()
+for epoch in range(1, n_epochs + 1):
+  train(epoch)
+  test()
+'''
+'''
+fig = plt.figure()
+plt.plot(train_counter, train_losses, color='blue')
+plt.scatter(test_counter, test_losses, color='red')
+plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+plt.xlabel('number of training examples seen')
+plt.ylabel('negative log likelihood loss')
+plt.show()
+fig
+'''
+
 with torch.no_grad():
-    logps = model(img)
+  output = network(example_data)
+fig = plt.figure()
+for i in range(6):
+  plt.subplot(2,3,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Prediction: {}".format(
+    output.data.max(1, keepdim=True)[1][i].item()))
+  plt.xticks([])
+  plt.yticks([])
+  plt.show()
+fig
 
-# Output of the network are log-probabilities, need to take exponential for probabilities
-ps = torch.exp(logps)
-probab = list(ps.numpy()[0])
-print("Predicted Digit =", probab.index(max(probab)))
-view_classify(img.view(1, 28, 28), ps)
